cece193df96fec40168289a5136009d459d1dc36
frkandris <p.toth.andras@gmail.com>
2025-03-16 00:15:07 +0100

    license stuff

--- print-button.php
+++ print-button.php
@@ -11,6 +11,21 @@
  * Domain Path: /languages
  */
 
+/*
+Print Button is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Print Button is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Print Button. If not, see http://www.gnu.org/licenses/gpl-2.0.txt.
+*/
+
 // If this file is called directly, abort.
 if (!defined('WPINC')) {
     die;
